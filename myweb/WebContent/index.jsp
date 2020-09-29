41320ea2fe27b14449c8d3aed78ca97012ff6f48
fanhu <fanhu@au1.ibm.com>
2020-09-29 15:11:51 +1000

    fix

--- myweb/WebContent/index.jsp
+++ myweb/WebContent/index.jsp
@@ -7,7 +7,7 @@
 <title>this is the demo message board</title>
 </head>
 <body><h3>
-<p5>container list (v0.2)</p5>
+<p5>container list (v0.3)</p5>
  </h3>
 	<table style="width: 70%; border-right-style: solid; border-top-width: thin; border-left-style: solid; border-right-width: thin; border-bottom-width: thin; border-bottom-style: solid; border-left-width: thin; border-top-style: solid; position: relative; clip: rect(auto, auto, auto, auto)">
 		<tr>
@@ -17,7 +17,7 @@
 		</tr>
 
 	<%@ page import ="demoweb.*" %>
-	<% Scan scan=new Scan(); java.util.ArrayList serverlist = scan.display(100); %>
+	<% Scan scan=new Scan(); java.util.ArrayList serverlist = scan.display(10); %>
 	<% for(int i = 0; i < serverlist.size(); i+=1) { %>
  		<tr> 
  			<td style="border-right-style: solid; border-left-style: solid; border-bottom-style: solid; border-top-style: solid; border-top-width: thin; border-right-width: thin; border-bottom-width: thin; border-left-width: thin"><%= i+1 %></td>
--- myweb/src/demoweb/Scan.java
+++ myweb/src/demoweb/Scan.java
@@ -21,24 +21,24 @@ public class Scan {
 		URL url;
 		//String status="initial";
 		ArrayList<String> status=new ArrayList();
-		System.out.println("v0.2 scan time is "+times);
+		System.out.println("v0.3 scan time is "+times);
 		
-		try {
-			Map<String, String> env = System.getenv();
-			String infurl=env.get("INFO_URL");
-			if (infurl==null)
-			{
-				infurl="http://127.0.0.1:8080/information/status";
-				System.out.println("Can't find INFO_URL from the environment property, using the default one "+infurl);
-			}
-			else
-			{
-				System.out.println("found INFO_URL from the environment property, using the default one "+infurl);
-			}
+		Map<String, String> env = System.getenv();
+		String infurl=env.get("INFO_URL");
+		if (infurl==null)
+		{
+			infurl="http://127.0.0.1:8080/information/status";
+			System.out.println("Can't find INFO_URL from the environment property, using the default one "+infurl);
+		}
+		else
+		{
+			System.out.println("found INFO_URL from the environment property, using the default one "+infurl);
+		}
 		
 			
-			for (int i=0;i<times;i++)
-			{
+		for (int i=0;i<times;i++)
+		{
+				/*
 				url = new URL(infurl);	
 				System.out.println("try No. "+i+" to connect "+url);
 				try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
@@ -53,11 +53,33 @@ public class Scan {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				*/
+
+			try {
+				ProcessBuilder processBuilder = new ProcessBuilder();
+	
+			    processBuilder.command("curl", infurl);
+	
+			    Process process = processBuilder.start();
+	
+			    try (BufferedReader reader = new BufferedReader(
+			            new InputStreamReader(process.getInputStream()))) {
+	
+			        String line;
+	
+			        while ((line = reader.readLine()) != null) {
+			        	System.out.println(line);
+			            status.add(line);
+			        }
+	
+			    }
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
+
 		
 		return status;
 	}
